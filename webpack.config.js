9c51144af2f05fbee6141956cbdbcab93ed9baa5
David <1337dallen@gmail.com>
2019-03-11 22:33:28 +1100

    Setup a better dev environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'dev/index.html'
+    })
+  ]
 };
